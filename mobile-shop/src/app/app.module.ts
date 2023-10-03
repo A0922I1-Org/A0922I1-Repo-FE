a627d50856e67925c87abdaa273402b0d2f1739a
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-10-03 14:23:32 +0700

    Binh-upload

--- mobile-shop/src/app/app.module.ts
+++ mobile-shop/src/app/app.module.ts
@@ -1,23 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// @ts-ignore
+import {BrowserModule} from '@angular/platform-browser';
+// @ts-ignore
+import {NgModule} from '@angular/core';
+// @ts-ignore
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+// @ts-ignore
 import {HttpClientModule} from "@angular/common/http";
+// @ts-ignore
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-
+import {BodyComponent} from './homepage/body/body.component';
+import {FooterComponent} from './homepage/footer/footer.component';
+import {HeaderComponent} from './homepage/header/header.component';
+import {ImageScrollComponent} from './homepage/image-scroll/image-scroll.component';
+// @ts-ignore
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    FooterComponent,
+    HeaderComponent,
+    ImageScrollComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbModule,
+    CarouselModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ mobile-shop/src/app/homepage/header/header.component.ts
@@ -0,0 +1,40 @@
+// @ts-ignore
+import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import {OwlOptions} from 'ngx-owl-carousel-o';
+// @ts-ignore
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() {}
+  bannerSlider: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  };
+  ngOnInit(): void {
+  }
+}
