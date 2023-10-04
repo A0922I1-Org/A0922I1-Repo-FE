531a6657421472f69e2e4241cb4d74ba35d47f67
T_Manh <manh.manh1611@gmail.com>
2023-10-04 18:42:12 +0700

    thêm module cho từng object

---
+++ mobile-shop/src/app/module/input-invoice/input-invoice.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InputInvoiceRoutingModule } from './input-invoice-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InputInvoiceRoutingModule
+  ]
+})
+export class InputInvoiceModule { }
