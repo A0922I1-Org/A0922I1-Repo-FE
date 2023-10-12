ebb34f60b762ffd332bdd2764fee0e56d824e9ef
thangvp1907 <thangvp1999@gmail.com>
2023-10-12 22:20:02 +0700

    update

--- mobile-shop/src/app/app-routing.module.ts
+++ mobile-shop/src/app/app-routing.module.ts
@@ -1,12 +1,31 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {AuthGuard} from "./auth.guard";
+import {RoleGuard} from "./role.guard";
 
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./model/security/security-routing.module').then(module => module.SecurityRoutingModule)},
-  {path: 'signUp', loadChildren: () => import('./model/user/user-routing.module').then(module => module.UserRoutingModule)},
-  {path: 'infor', loadChildren: () => import('./model/user-detail/infor-user-routing.module').then(module => module.InforUserRoutingModule)},
-  {path: 'changePassword', loadChildren: () => import('./model/user-detail/infor-user-routing.module').then(module => module.InforUserRoutingModule)},
+  {
+    path: 'login',
+    loadChildren: () => import('./model/security/security-routing.module').then(module => module.SecurityRoutingModule),
+  },
+  {
+    path: 'signUp',
+    loadChildren: () => import('./model/user/user-routing.module').then(module => module.UserRoutingModule),
+    canActivate: [AuthGuard,RoleGuard], // Áp dụng AuthGuard cho route này
+    data: { roles: ['ADMIN']}
+
+  },
+  {
+    path: 'infor',
+    loadChildren: () => import('./model/user-detail/infor-user-routing.module').then(module => module.InforUserRoutingModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'changePassword',
+    loadChildren: () => import('./model/user-detail/infor-user-routing.module').then(module => module.InforUserRoutingModule),
+    canActivate: [AuthGuard], // Áp dụng cả AuthGuard và RoleGuard cho route này
+  },
 ];
 
 @NgModule({
