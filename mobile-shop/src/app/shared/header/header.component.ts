e7a1d01afca116310033d63de4729125c8b1a01e
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-10-16 19:58:36 +0700

    BinhPHP - done search fucntion

--- mobile-shop/src/app/shared/header/header.component.ts
+++ mobile-shop/src/app/shared/header/header.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {OwlOptions} from "ngx-owl-carousel-o";
-
+import {SearchService} from '../../service/search.service';
+import { SharedDataService } from '../../service/shared-data.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  searchQuery: string = '';
+  constructor(private searchService: SearchService, private sharedDataService: SharedDataService) { }
   bannerSlider: OwlOptions = {
     loop: true,
     mouseDrag: true,
@@ -35,5 +36,11 @@ export class HeaderComponent implements OnInit {
   };
   ngOnInit(): void {
   }
-
+  onSearch() {
+    if (this.searchQuery.trim() !== '') {
+      this.searchService.search(this.searchQuery).subscribe(results => {
+        this.sharedDataService.updateSearchResults(results);
+      });
+    }
+  }
 }
