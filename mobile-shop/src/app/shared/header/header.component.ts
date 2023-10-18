b0e9b6b29c098611102f0989ffcd46c5d2740ce7
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-10-18 20:54:35 +0700

    modify code

--- mobile-shop/src/app/shared/header/header.component.ts
+++ mobile-shop/src/app/shared/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {OwlOptions} from "ngx-owl-carousel-o";
 import {SearchService} from '../../service/search.service';
 import { SharedDataService } from '../../service/shared-data.service';
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,30 +10,6 @@ import { SharedDataService } from '../../service/shared-data.service';
 export class HeaderComponent implements OnInit {
   searchQuery: string = '';
   constructor(private searchService: SearchService, private sharedDataService: SharedDataService) { }
-  bannerSlider: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
-    nav: true
-  };
   ngOnInit(): void {
   }
   onSearch() {
