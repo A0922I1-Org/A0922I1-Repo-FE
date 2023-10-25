04d4b839d9344a9d2f626ea8431647ef9717d431
maivanthuan <thuan31102003@gmail.com>
2023-10-25 19:50:56 +0700

    update

--- mobile-shop/src/app/app-routing.module.ts
+++ mobile-shop/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {AuthGuard} from './auth.guard';
 import {RoleGuard} from './role.guard';
-import {HomePageComponent} from "./shared/home-page/home-page.component";
+import {HomePageComponent} from './shared/home-page/home-page.component';
 
 
 const routes: Routes = [
@@ -14,7 +14,7 @@ const routes: Routes = [
   {
     path: 'signUp',
     loadChildren: () => import('./model/user/user-routing.module').then(module => module.UserRoutingModule),
-    canActivate: [AuthGuard, RoleGuard],// Áp dụng AuthGuard cho route này
+    canActivate: [AuthGuard, RoleGuard], // Áp dụng AuthGuard cho route này
     data: {roles: ['ADMIN']}
   },
   {
@@ -30,17 +30,19 @@ const routes: Routes = [
   {
     path: 'product',
     loadChildren: () => import('./module/product/product.module').then(module => module.ProductModule),
-    canActivate: [AuthGuard],
-  {
-    path: 'product',
-    loadChildren: () => import('./module/product/product.module').then(module => module.ProductModule),
-    // canActivate: [AuthGuard,RoleGuard],
-    // data: { roles: ['ADMIN','BUSINESS','SALE',"STORAGE"]}
-
+    canActivate: [AuthGuard]
   },
+  // {
+  //   path: 'product',
+  //   loadChildren: () => import('./module/product/product.module').then(module => module.ProductModule),
+  //   // canActivate: [AuthGuard,RoleGuard],
+  //   // data: { roles: ['ADMIN','BUSINESS','SALE',"STORAGE"]}
+  //
+  // },
   {
-    path: 'api/managerPurchaseHistory',
-    loadChildren: () => import('./module/manager-purchase-history/manager-purchase-history.module').then(module => module.ManagerPurchaseHistoryModule),
+    path: 'managerPurchaseHistory',
+    loadChildren: () => import('./module/manager-purchase-history/manager-purchase-history.module')
+      .then(module => module.ManagerPurchaseHistoryModule),
     canActivate: [AuthGuard, RoleGuard],
     data: {roles: ['ADMIN', 'BUSINESS']}
   },
