f93e6c59532ae5bc3ac3f91ab338b3357db9246e
hnghia1902 <102527569+hnghia1902@users.noreply.github.com>
2023-10-27 15:25:14 +0700

    sua xuong dong addrees

--- mobile-shop/src/app/module/supplier/create-supplier/create-supplier.component.ts
+++ mobile-shop/src/app/module/supplier/create-supplier/create-supplier.component.ts
@@ -16,7 +16,7 @@ export class CreateSupplierComponent implements OnInit {
   errorData: Map<String, string[]> = new Map();
   textLower: string = '';
   public phoneVN = /^(0[1-9]\d{8})$/;
-  public vietnamese = /^[a-zA-Z0-9-()*_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ ]+$/;
+  public vietnamese = /^[a-zA-Z0-9-()*_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ \n]+$/;
   public emailRegex = /^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,}$/;
   @ViewChild('successNotification') successNotification: ElementRef;
 
